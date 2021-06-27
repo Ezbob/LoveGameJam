c10e919f6a38eb1dcb596c93b26258c119ea99db
Anders Busch <andersbusch@gmail.com>
2021-06-27 10:14:19 +0200

    smaller hit boxes

--- char/PunkChar.lua
+++ char/PunkChar.lua
@@ -10,11 +10,10 @@ function PunkChar:new(o)
   self.__index = self
 
   r:addHitbox("body", 25, 50, 25, 50)
-  r:addHitbox("punch_right", 55, 54, 24, 20)
-  r:addHitbox("punch_left", -3, 54, 24, 20)
-  r:addHitbox("kick_right", 55, 77, 28, 20)
-  r:addHitbox("kick_left", -7, 77, 28, 20)
-
+  r:addHitbox("punch_right", 62, 56, 12, 12)
+  r:addHitbox("punch_left", 2, 56, 12, 12)
+  r:addHitbox("kick_right", 57, 71, 20, 12)
+  r:addHitbox("kick_left", -2, 71, 20, 12)
   return r
 end
 
