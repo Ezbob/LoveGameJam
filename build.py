c6004bfa4ce55d8c5a6872ca9344a86036e99876
Anders Busch <andersbusch@gmail.com>
2021-07-02 22:06:54 +0200

    configurable love2d

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import pathlib
 import zipfile
 import shutil
 import platform
+import argparse
 
 
 THIS_DIR = pathlib.Path(".")
@@ -30,6 +31,10 @@ DIST_FILE = THIS_DIR / "game"
 
 if __name__ == "__main__":
 
+	parser = argparse.ArgumentParser(description="builds distribution")
+	parser.add_argument('-l', '--love2d', type=pathlib.Path, default=pathlib.Path(shutil.which('love')))
+	args = parser.parse_args()
+
 	with zipfile.ZipFile(OUTPUT_FILE.as_posix(), mode='w', compresslevel=9) as zfout:
 		for p in ASSETS_FILES.iterdir():
 			if p.suffix != ".aseprite" and p.suffix != ".tmx":
@@ -50,7 +55,7 @@ if __name__ == "__main__":
 					zfout.write(src)
 
 	# Taken from PATH
-	lovepath = shutil.which('love')
+	lovepath = args.love2d
 
 	if lovepath is None:
 		raise ValueError("Could not infer love2d executable path")
